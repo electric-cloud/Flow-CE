bc7e49649762cf27afe29dc127dbeb794eede3f1
Artyom Trityak <art.trityak@gmail.com>
2016-03-14 15:17:14 -0700

    Fix descriptions

--- on-boarding-video/javascript/videos-data.js
+++ on-boarding-video/javascript/videos-data.js
@@ -6,7 +6,7 @@ export default [
     title: 'Welcome to ElectricFlow Community Edition',
     videoUrl: 'https://www.youtube.com/embed/GLIG-9c8r3s',
     helpcenterUrl: 'https://helpcenter.electric-cloud.com/hc/en-us/articles/205156666?page=1',
-    description: `1In this video you will learn how to get started with Flow Community Edition including steps to download, 
+    description: `1. In this video you will learn how to get started with Flow Community Edition including steps to download,
           start and register your account in the virtual appliance.
           `
 
@@ -16,7 +16,7 @@ export default [
     title: 'ElectricFlow Deploy Concepts',
     videoUrl: 'https://www.youtube.com/embed/8KhsyWnFWNc',
     helpcenterUrl: 'https://helpcenter.electric-cloud.com/hc/en-us/articles/205270293?page=1',
-    description: `2This module provides a conceptual overview of ElectricFlow Deploy with an introduction to the model based approach
+    description: `2. This module provides a conceptual overview of ElectricFlow Deploy with an introduction to the model based approach
           to defining applications and environments so that you can get automated, repeatable and error-free deployments.
           `
 
@@ -26,27 +26,27 @@ export default [
     title: 'Your First Deployment',
     videoUrl: 'https://www.youtube.com/embed/ILz8yf8_64k',
     helpcenterUrl: 'https://helpcenter.electric-cloud.com/hc/en-us/articles/204966796?page=1',
-    description: `3In this module you will run your first deployment of a “Hello World” application.
+    description: `3. In this module you will run your first deployment of a “Hello World” application.
           `
 
   },
   {
     id: 3,
-    title: ‘Deploying a Typical J2EE Application’,
+    title: 'Deploying a Typical J2EE Application',
     videoUrl: 'https://www.youtube.com/embed/eA_pnhkUVyM',
     helpcenterUrl: 'https://helpcenter.electric-cloud.com/hc/en-us/articles/204966906?page=1',
-    description: `4This video shows you how to model and deploy an n-tiered J2EE application with components 
+    description: `4. This video shows you how to model and deploy an n-tiered J2EE application with components
           that get deployed to different target servers such as a web server, application server and database.
           `
 
   },
   {
     id: 4,
-    title: ‘Deploying Your Own Application’,
+    title: 'Deploying Your Own Application',
     videoUrl: 'https://www.youtube.com/embed/-BKdG_ZrL3s',
     helpcenterUrl: 'https://helpcenter.electric-cloud.com/hc/en-us/articles/204968766?page=1',
-    description: `5This module shows you how to build an application model from scratch and deploy it to the JBoss (WildFly) 
-          server running within the appliance.  The prerequisite is that the artifacts to be deployed already exist in a 
+    description: `5. This module shows you how to build an application model from scratch and deploy it to the JBoss (WildFly)
+          server running within the appliance.  The prerequisite is that the artifacts to be deployed already exist in a
           Nexus repository accessible from the appliance.
           `
 
