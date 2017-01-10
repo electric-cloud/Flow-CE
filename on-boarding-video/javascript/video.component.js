01e2b49b67ca01abeb684b40f110b1c7492a68ab
Artyom Trityak <art.trityak@gmail.com>
2017-01-10 14:36:49 -0800

    User name in onboarding URL

--- on-boarding-video/javascript/video.component.js
+++ on-boarding-video/javascript/video.component.js
@@ -4,6 +4,11 @@ import React from 'react';
 
 
 let VideoComponent = ({title, description, videoUrl, helpcenterUrl}) => {
+  let userId = document.location.hash.match(/user_id=(.+)/);
+  if (userId && userId.length) {
+    userId = userId[1];
+  }
+
   return (
     <div className="ec-videos__video">
       <div className="ec-videos__video-header" title={title}>{title}</div>
@@ -11,7 +16,7 @@ let VideoComponent = ({title, description, videoUrl, helpcenterUrl}) => {
         <iframe width="100%" height="100%" src={videoUrl} allowFullScreen={true}></iframe>
       </div>
       <div className="ec-videos__video-description">
-        {description} - <a href={helpcenterUrl} target="_blank">Full tutorial &gt;</a>
+        {description} - <a href={`${helpcenterUrl}#user_id=${userId}`} target="_blank">Full tutorial &gt;</a>
       </div>
     </div>
   );
